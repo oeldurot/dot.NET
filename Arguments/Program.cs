6ae778437f6788bca88fafb6f92fa64b04ca98ac
.öldurot <iflybkeir2000@mail.ru>
2025-09-20 13:11:49 +0300

    Arguments in console app

---
+++ Arguments/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace Arguments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"There are {args.Length} arguments");
+
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+        }
+    }
+}
