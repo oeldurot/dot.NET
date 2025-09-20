b26d11fda09eefbf28ba90c8816fa45c56e0afb9
.öldurot <iflybkeir2000@mail.ru>
2025-09-20 13:48:09 +0300

    Parameters for console app in arguments

--- Arguments/Program.cs
+++ Arguments/Program.cs
@@ -13,6 +13,20 @@ namespace Arguments
                 Console.WriteLine(arg);
             }
 
+
+            if (args.Length < 3)
+            {
+                WriteLine("Ты должен выбрать два цвета и размер курсора");
+                WriteLine("dotnet run red yellow 50");
+                return;
+            }
+
+            ForegroundColor = (ConsoleColor)Enum.Parse(enumType: typeof(ConsoleColor), value: args[0], ignoreCase: true);
+
+            BackgroundColor = (ConsoleColor)Enum.Parse(enumType: typeof(ConsoleColor), value: args[1], ignoreCase: true);
+
+            CursorSize = int.Parse(args[2]);
+
         }
     }
 }
