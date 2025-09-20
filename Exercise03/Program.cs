14a7f59f57703f633365105194ed25690e473aa7
.öldurot <iflybkeir2000@mail.ru>
2025-09-20 18:22:09 +0300

    Exercise_03

---
+++ Exercise03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Console;
+using static System.Math;
+
+
+namespace Exercise03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //type
+            //bytes of memory
+            //min value
+            //max value
+            WriteLine($"Type");
+            WriteLine("___________________________________________________________");
+            WriteLine($"sbyte {sizeof(sbyte)} {sbyte.MinValue} {sbyte.MaxValue}");
+            WriteLine($"byte  {sizeof(byte)} {byte.MinValue} {byte.MaxValue}");
+            WriteLine($"short {sizeof(short)} {short.MinValue} {short.MaxValue}");
+            WriteLine($"ushort {sizeof(ushort)} {ushort.MinValue} {ushort.MaxValue}");
+            WriteLine($"int {sizeof(int)} {int.MinValue} {int.MaxValue}");
+            WriteLine($"uint {sizeof(uint)} {uint.MinValue} {uint.MaxValue}");
+            WriteLine($"long {sizeof(long)} {long.MinValue} {long.MaxValue}");
+            WriteLine($"ulong {sizeof(ulong)} {ulong.MinValue} {ulong.MaxValue}");
+            WriteLine($"float {sizeof(float)} {float.MinValue} {float.MaxValue}");
+            WriteLine($"double {sizeof(double)} {double.MinValue} {double.MaxValue}");
+            WriteLine($"decimal {sizeof(decimal)} {decimal.MinValue} {decimal.MaxValue}");
+        }
+    }
+}
