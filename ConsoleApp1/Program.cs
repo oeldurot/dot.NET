b8e3f0201b610f990d5da17c3d2ddbe4d72f50bf
.öldurot <iflybkeir2000@mail.ru>
2025-09-20 12:50:51 +0300

    test commit 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Press any key combination: ");
+            Console.Write("Hello Yan, press any key combination: ");
             ConsoleKeyInfo key = Console.ReadKey();
             Console.WriteLine();
             Console.WriteLine("Key: {0}, Char: {1}, Modifiers: {2}", arg0: key.Key, arg1: key.KeyChar, arg2: key.Modifiers);
