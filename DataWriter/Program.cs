6bf8dad4de2cf6b1b841f683913aa1860a56b698
.öldurot <iflybkeir2000@mail.ru>
2025-11-05 01:28:45 +0300

    DataWriter

---
+++ DataWriter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DataWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your email...");
+            string email = Console.ReadLine();
+            Console.WriteLine("Write your password...");
+            string password = Console.ReadLine();
+            Console.WriteLine("Write your path...");
+            string path = Console.ReadLine();
+
+            string email1 = new string(email);
+            string password1 = new string(password);
+            string path1 = new string(path);
+
+            Console.WriteLine($"{email1},{password1},{path1}");
+
+        }
+
+        extern AppendAllLines(string email1, IEnumerable<string> email1, UTF7Encoding email1);
+    }
+
+}
---
+++ DataWriter1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime;
+
+namespace DataWriter1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your email...");
+            var email = Console.ReadLine();
+            Console.WriteLine("Write your password...");
+            var password = Console.ReadLine();
+            Console.WriteLine("Write your path...");
+            var path = Console.ReadLine();
+
+            var email1 = new string(email);
+            var password1 = new string(password);
+            var path1 = new string(path);
+
+
+            Program Program1 = new ();
+            //Console.WriteLine($"{email1},{password1},{path1}");
+        }
+
+        public class Program1
+        { 
+            public static void Create(string path1, System.Collections.Generic.IEnumerable<string> contents, UTF32Encoding encoding)
+            {
+                using (FileStream fs = File.Create(path1));
+            }
+        }    
+
+    }
+}
