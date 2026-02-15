1a6bfe3840c697b8ba417172cb882f2d3c5e068d
.öldurot <iflybkeir2000@mail.ru>
2026-02-16 02:32:15 +0300

    Selection and iteration statements

---
+++ IterationStatements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            string? password;
+            int count = 0;
+
+            for (int y = 1; y <= 10; y++)
+            {
+                Console.WriteLine(y);
+            }
+
+            while (x<=10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+            do
+            {
+                Console.Write("Enter your password: ");
+                password = Console.ReadLine();
+                count++;
+                if (count == 10)
+                {
+                    Console.WriteLine("Вы использовали все попытки ввода пароля!");
+                    break;
+                }
+            }
+            while (password != "248609");
+
+        }
+    }
+}
---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace SelectionStatements
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Games";
+
+            Console.WriteLine("Press R for read-only or W for writeable: "); //выбор read-only or writeable
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.WriteLine();
+
+            Stream? s;
+
+            if (key.Key == ConsoleKey.R) //если введенная с клавиатуры клавиша равняется клавише R or W
+            {
+                s = File.Open(Path.Combine(path, "file.txt"), FileMode.Open, FileAccess.Read);
+            }   //комбинируется путь и имя конечного файла, создается или открывается файл с заданным именем и типом доступа к файлу
+            else
+            {
+                s = File.Open(Path.Combine(path, "file.txt"), FileMode.OpenOrCreate, FileAccess.Write);
+            }   //комбинируется путь и имя конечного файла, создается или открывается файл с заданным именем и типом доступа к файлу
+
+            string message;
+
+            switch (s)
+            {
+                case FileStream writeableFile when s.CanWrite:
+                    message = "The stream is a file that I can write to.";
+                    break;
+
+                case FileStream readOnlyFile when s.CanRead:
+                    message = "The stream is a read-only file.";
+                    break;
+                case MemoryStream ms:
+                    message = "The stream is a memory address";
+                    break;
+
+                default:
+                    message = "The stream is some other type";
+                    break;
+
+                case null:
+                    message = "The stream is null";
+                    break;
+            }
+            Console.WriteLine(message);
+
+        }
+    }
+}
