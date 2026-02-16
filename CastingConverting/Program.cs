59a7e1558a10bee244f87303a6ce415f1c4dec85
.öldurot <iflybkeir2000@mail.ru>
2026-02-16 22:41:31 +0300

    CastingConverting

---
+++ CastingConverting/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using static System.Convert;
+
+namespace CastingConverting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = a;
+            //Console.WriteLine(b);
+
+            double c = 9.8;
+            int d = (int)c;
+            //Console.WriteLine(d);
+
+            long e = 5_000_000_000;
+            int f = (int)e;
+            Console.WriteLine($"e is {e:N0} and f is {f:N0}");
+            e = long.MaxValue;
+            f = (int)e;
+            Console.WriteLine($"e is {e:N0} and f is {f:N0}");
+
+            double g = 9.8;
+            int h = ToInt32(g);
+            Console.WriteLine($"g is {g} and h is {h}");
+
+            double[] doubles = new[]
+                {9.49, 9.5, 9.51, 10.49, 10.5, 10.51};
+
+            foreach (double n in doubles)
+            {
+                Console.WriteLine($"ToInt32({n}) is {ToInt32(n)}"); 
+            }
+
+
+
+        }
+    }
+}
+
